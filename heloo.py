14de0a74d148fcfc1753dedea19464c71bb0d63e
Akash1435 <34570234+Akash1435@users.noreply.github.com>
2018-06-11 12:43:51 +0530

    Add files via upload

---
+++ heloo.py
@@ -0,0 +1,6 @@
+Python 3.4.4rc1 (v3.4.4rc1:04f3f725896c, Dec  6 2015, 16:42:12) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> i=0
+n = input()
+for i in range(n):
+	print '\nHello'

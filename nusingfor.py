825f8206556d8477a811f2ca6840d003d291a3b1
Akash1435 <34570234+Akash1435@users.noreply.github.com>
2018-06-11 12:35:41 +0530

    Update nusingfor.py

--- nusingfor.py
+++ nusingfor.py
@@ -1,5 +1,4 @@
-Python 3.4.4rc1 (v3.4.4rc1:04f3f725896c, Dec  6 2015, 16:42:12) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
+
 >>> n = input('enter the number')
 i=1
 s=0

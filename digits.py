0f150678168f304e8bb26105d5791ad891da6f16
Akash1435 <34570234+Akash1435@users.noreply.github.com>
2018-06-11 12:52:28 +0530

    Update digits.py

--- digits.py
+++ digits.py
@@ -1,10 +1,7 @@
-Python 3.4.4rc1 (v3.4.4rc1:04f3f725896c, Dec  6 2015, 16:42:12) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> i = input('enter a number')
+i = input()
 i = int(i)
 count = 0
 def countj(i):
-	print(i)
 	global count
 	while(i>0):
 		count = count+1

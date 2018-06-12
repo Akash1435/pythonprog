4a1b5da83e4e42457bcb42ad82d5f1d78691537b
Akash1435 <34570234+Akash1435@users.noreply.github.com>
2018-06-12 14:53:10 +0530

    Add files via upload

---
+++ holiday.py
@@ -0,0 +1,5 @@
+mystr = str(raw_input())
+if(mystr == 'SATURDAY' or mystr == 'Saturday' or mystr == 'saturday' or mystr == 'SUNDAY' or mystr == 'Sunday' or mystr == 'sunday'):
+	print 'yes'
+else: 
+	print 'no'
